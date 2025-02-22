047a27287a4c8bd865896845084d9b4a3d9c1f15
Иван <ivan11-90@mail.ru>
2025-02-22 11:32:50 +0500

    Добавить новые возможности по ТЗ 8-го спринта

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,51 +10,6 @@ import main.java.tasks.TaskStatus;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
-        TaskManager taskManager = Managers.getDefault();
-
-        Task task1 = new Task("Задача 1", "Описание 1", TaskStatus.NEW);
-        taskManager.addTask(task1);
-        Task task2 = new Task("Задача 2", "Описание 2", TaskStatus.NEW);
-        taskManager.addTask(task2);
-        Epic epic1 = new Epic("Эпик1", "Описание1");
-        taskManager.addEpic(epic1);
-        Subtask subtask1 = new Subtask("Подзадача1", "Описание1", TaskStatus.NEW, 3);
-        Subtask subtask2 = new Subtask("Подзадача2", "Описание2", TaskStatus.NEW, 3);
-        Subtask subtask3 = new Subtask("Подзадача3", "Описание3", TaskStatus.NEW, 3);
-        taskManager.addSubtask(subtask1);
-        taskManager.addSubtask(subtask2);
-        taskManager.addSubtask(subtask3);
-        Epic epic2 = new Epic("Эпик2", "Описание2");
-        taskManager.addEpic(epic2);
-
-        taskManager.getTaskById(2);
-        taskManager.getSubTaskById(5);
-        taskManager.getEpicById(7);
-        taskManager.getTaskById(1);
-        taskManager.getSubTaskById(4);
-        taskManager.getSubTaskById(6);
-        taskManager.getEpicById(3);
-        taskManager.getTaskById(1);
-        taskManager.getSubTaskById(5);
-        taskManager.getTaskById(2);
-        taskManager.getTaskById(2);
-        System.out.println(taskManager.getHistory());
-        System.out.println("_".repeat(20));
-
-        taskManager.removeTask(1);
-        System.out.println(taskManager.getHistory());
-        System.out.println("_".repeat(20));
-
-        taskManager.removeSubtask(5);
-        System.out.println(taskManager.getHistory());
-        System.out.println("_".repeat(20));
-
-        taskManager.removeEpic(3);
-        System.out.println(taskManager.getHistory());
-        System.out.println("_".repeat(20));
-
-        printAllTasks(taskManager);
 
     }
 
